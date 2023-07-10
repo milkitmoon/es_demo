3bf4937640766ad079ae7262a88ebe822cf6b706
milkitmoon <milkit.moon@gmail.com>
2023-07-10 16:37:20 +0900

    UserController 에서 UserHandlerServiceImpl  을 호출하도록 수정

--- src/main/java/com/milkit/app/api/user/UserController.java
+++ src/main/java/com/milkit/app/api/user/UserController.java
@@ -1,49 +1,47 @@
 package com.milkit.app.api.user;
 
-import java.util.List;
-
+import com.milkit.app.domain.user.service.UserHandlerServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.milkit.app.api.AbstractApiController;
 import com.milkit.app.common.response.GenericResponse;
 import com.milkit.app.domain.user.User;
-import com.milkit.app.domain.user.service.UserServiceImpl;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@Tag(description = "2. 사용자 정보", name = "UserController")
+@Tag(description = "1. 사용자 정보", name = "UserController")
 public class UserController extends AbstractApiController {
 
-    @Autowired
-    private UserServiceImpl userService;
+    private UserHandlerServiceImpl userHandlerService;
 
     @PostMapping("/api/user/signup")
-    @Operation(summary = "2.1 회원가입", description = "회원정보를 등록한다.")
+    @Operation(summary = "1.1 회원가입", description = "회원정보를 등록한다.")
     public ResponseEntity<GenericResponse<User>> signup(
         @Parameter(name = "회원정보", required = true) @RequestBody final User request) throws Exception {
-        User newUser = userService.signup(request);
-        return apiResponse(() -> newUser);
+        return apiResponse(() -> userHandlerService.signup(request));
     }
 
     @GetMapping("/api/user")
     @PreAuthorize("hasAnyRole('ROLE_MEMBER','ROLE_ADMIN')")
-    @Operation(summary = "2.2 사용자정보 전체조회 ", description = "사용자정보 전체 목록을 조회한다.")
-    public ResponseEntity<GenericResponse<List<User>>> userinfo() throws Exception {
-        return apiResponse(() -> userService.selectAll());
+    @Operation(summary = "1.2 사용자정보 조건조회 ", description = "사용자정보 전체 목록을 조회한다.")
+    public ResponseEntity<GenericResponse<Page<User>>> userinfo(
+            User user, @PageableDefault(page = 0, size = 10) Pageable pageable) throws Exception {
+        return apiResponse(() -> userHandlerService.searchByCondition(user, pageable));
     }
 
-    public void setUserService(UserServiceImpl userService) {
-        this.userService = userService;
+    @Autowired
+    public void setUserHandlerService(UserHandlerServiceImpl userHandlerService) {
+        this.userHandlerService = userHandlerService;
     }
 }
