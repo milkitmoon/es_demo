8362fea9c44dac4d3a22896b8c322ecce9b1c85f
milkitmoon <milkit.moon@gmail.com>
2023-07-10 16:31:34 +0900

    UserRepository 에 findByIdIn 추가

--- src/main/java/com/milkit/app/domain/user/repository/UserRepository.java
+++ src/main/java/com/milkit/app/domain/user/repository/UserRepository.java
@@ -2,10 +2,16 @@ package com.milkit.app.domain.user.repository;
 
 import com.milkit.app.domain.user.User;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUserId(String userId);
+
+	Page<User> findByIdIn(List<Long> ids, Pageable pageable);
 }
